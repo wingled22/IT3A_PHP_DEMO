a0f13400cc66210fcc025536851148e7ec0c8e80
wingled22 <pelayowindel@gmail.com>
2022-03-02 12:12:50 +0800

    added table of books on index page

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
     require "db_connect.php";
+?>
 
+<link rel="stylesheet" href="css/site.css">
+
+<table>
+    <thead>
+        <th>Name</th>
+        <th>Author</th>
+    </thead>
+    <tbody>
+<?php
     $sql = "SELECT * from book";
     $res = $conn->query($sql);
 
     if(!$res)
         echo "no data on the the book table";
 
-    $row = $res -> fetch_assoc();
-    printf ("%s (%s)\n", $row["Name"], $row["Author"]);
-    
+
+    while($row =mysqli_fetch_object($res)){
 ?>
+    <tr>
+        <td><?php echo $row->Name?></td>
+        <td><?php echo $row->Author?></td>
+    </tr>
+<?php
+    }    
+?>
+    </tbody>
+</table>
+
