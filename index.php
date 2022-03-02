e5e401fcebeb2abc917f4b9aa18262198187c82a
wingled22 <pelayowindel@gmail.com>
2022-03-02 11:54:04 +0800

    merge

--- index.php
+++ index.php
@@ -1,38 +1,13 @@
 <?php
-
     require "db_connect.php";
+
+    $sql = "SELECT * from book";
+    $res = $conn->query($sql);
+
+    if(!$res)
+        echo "no data on the the book table";
+
+    $row = $res -> fetch_assoc();
+    printf ("%s (%s)\n", $row["Name"], $row["Author"]);
     
-    /*adding data to the book table*/
-    // $sqlQuery = "INSERT INTO book(Name, Author) VALUES ('Book Name1','Book Author1')";
-    // $res = $conn->query($sqlQuery);
-    // if($res)
-    //     echo "new data added";
-    // else 
-    //     echo "error";
-
-    /*
-        updating entry to the book table
-    */ 
-    // $sqlQuery = "UPDATE book SET Name='Updated Name', Author ='Updated Author' WHERE ID = 8";
-
-    // $res = $conn->query($sqlQuery);
-    
-    // if($res)
-    //     echo "data updated";
-    // else 
-    //     echo "error";
-
-
-     /*
-        Deleting entry to the book table
-    */ 
-    // $sqlQuery = "DELETE FROM book WHERE ID = 8";
-    // $res = $conn->query($sqlQuery);
-    // if($res)
-    //     echo "data Deleted";
-    // else 
-    //     echo "error";
-
- ?>
-
-
+?>
