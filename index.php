72862e2d806da2a95c5ae34625791060fa493808
wingled22 <pelayowindel@gmail.com>
2022-03-02 11:11:48 +0800

    added first files

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+    require "db_connect.php";
+    
+    /*adding data to the book table*/
+    // $sqlQuery = "INSERT INTO book(Name, Author) VALUES ('Book Name1','Book Author1')";
+    // $res = $conn->query($sqlQuery);
+    // if($res)
+    //     echo "new data added";
+    // else 
+    //     echo "error";
+
+    /*
+        updating entry to the book table
+    */ 
+    // $sqlQuery = "UPDATE book SET Name='Updated Name', Author ='Updated Author' WHERE ID = 8";
+
+    // $res = $conn->query($sqlQuery);
+    
+    // if($res)
+    //     echo "data updated";
+    // else 
+    //     echo "error";
+
+
+     /*
+        Deleting entry to the book table
+    */ 
+    // $sqlQuery = "DELETE FROM book WHERE ID = 8";
+    // $res = $conn->query($sqlQuery);
+    // if($res)
+    //     echo "data Deleted";
+    // else 
+    //     echo "error";
+
+ ?>
+
+
---
+++ neededbyindex.php
@@ -0,0 +1,5 @@
+<?php
+    echo "i am needed by index.php" ;
+?>
+
+<div style="height : 100px; background: black;"></div>
